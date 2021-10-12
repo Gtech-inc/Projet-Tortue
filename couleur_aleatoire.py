af7bc5bef7d72af69712ec6af8c782814690f657
Rio <Nesta.loisy@gmail.com>
2021-10-12 12:25:27 +0200

    add turle.pencolor()

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -6,5 +6,5 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     '''
-    return randint(0, 255), randint(0, 255), randint(0, 255)
+    return turtle.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
     pass
