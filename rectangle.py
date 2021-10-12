d78dafd6bd5e46d531a192e3ee72f38d7861dd7a
sf3868 <geoffrey.bousseau@stfrancoislaroche.fr>
2021-10-12 12:38:24 +0200

    J'ai fini rectangle !!!

--- rectangle.py
+++ rectangle.py
@@ -1,19 +1,27 @@
 import turtle
 
-def rectangle(x,y,w,h):
-    '''
+
+def rectangle(x: int, y: int, w: int, h: int):
+    """
     Paramètres
         x, y : coordonnées du centre de la base de rectangle
         w : largeur du rectangle
         h : hauteur du rectangle
     Cette fonction dessine un rectangle Le point de coordonnées (x,y) est
     sur le côté en bas au milieu
-    '''
-
-    pass
+    """
+    if turtle.isdown():
+        turtle.penup()
+    turtle.setpos(x, y)
+    turtle.pendown()
+    turtle.goto(x + w, y)
+    turtle.goto(x, y - h)
+    turtle.goto(x, y - h)
+    turtle.goto(x, y)
+    turtle.penup()
 
 
 if __name__ == '__main__':
-    rectangle(0,0,150,100)
-    # On ferme la fenêtre s'il y a un clique gauche
+    rectangle(0, 0, 150, 100)
+    # On ferme la fenêtre si il y a un clique gauche
     turtle.exitonclick()
