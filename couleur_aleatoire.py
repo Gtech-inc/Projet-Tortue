008ad77b4d5ee93258f14b4eb90c480f20fc76ad
sf3868 <geoffrey.bousseau@stfrancoislaroche.fr>
2021-10-12 12:04:35 +0200

    First Update

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -6,5 +6,4 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     '''
-    print("Nesta tu pu")
     pass
