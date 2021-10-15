ed0b5567d0e3af7f9f6c6c0802215feb1108e833
Rio <Nesta.loisy@gmail.com>
2021-10-15 15:40:14 +0200

    commit immeuble

--- immeuble.py
+++ immeuble.py
@@ -12,16 +12,6 @@ def immeuble(x, y_sol):
         y_sol : ordonnée du sol du la rue
     Cette fonction dessine un immeuble Le nombre d'étage est compris aléatoirement entre 0 et 4
     La couleur de la façade et la couleur de la porte sont tirées au hasard
-<<<<<<< HEAD
-    '''
-    turtle.setx(x)
-    turtle.sety(y_sol)
-    # Nombre d'étage (aléatoire)
-    nbStage = randint(0, 7)
-
-    #Couleurs des éléments (aléatoire)
-    couleur = couleur_aleatoire()
-=======
     """
     # Nombre d'étage (aléatoire)
     nbStage = randint(0, 7)
@@ -31,24 +21,15 @@ def immeuble(x, y_sol):
     color1 = 244
 
     couleur = turtle.color(color1)
->>>>>>> 6856f838d3dd33cea2eece4e993b52006c651a19
 
     # Dessin du RDC
 
     # Dessin des étages
-<<<<<<< HEAD
     y_sol= -200
     x = -300
     for niveau in range(nbStage):
         etage(x, y_sol, couleur, niveau)
         y_sol += 75
-=======
-    y = -200
-    x = -300
-    for niveau in range(nbStage):
-        etage(x, y_sol, couleur, niveau)
-        y += 75
->>>>>>> 6856f838d3dd33cea2eece4e993b52006c651a19
 
     # Dessin du toit
     
