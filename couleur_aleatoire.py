28c4f2e918421b1e36874c4d1034cd9e2fdc349f
gb85150 <geoffrey.bousseau@gmail.com>
2021-10-18 00:06:08 +0200

    Ne marche pas en mode RGB :-1: , Réglage module de choix de couleur en mode str

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -7,7 +7,7 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     """
-    outputformat = 'RGB' # RGB or return a string
+    outputformat = 'test' # RGB or return a string
     if outputformat == 'RGB':
         RGB = (randint(0,256),randint(0,256),randint(0,256))
         return RGB
