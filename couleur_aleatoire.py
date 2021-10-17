f1bdae1e11950aa9852b5dcb0427befa214a3456
gb85150 <geoffrey.bousseau@gmail.com>
2021-10-18 00:01:31 +0200

    Patch couleur aléatoire
    + Update couleur_aléatoire()
    le programme semble fonctionner sous python 3.10
    + Update pycache

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -7,5 +7,14 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     """
-    colors = ['white', 'green', 'blue', 'cyan', 'red', 'yellow', 'orange']
-    return colors[randint(0, len(colors) - 1)]
+    outputformat = 'RGB' # RGB or return a string
+    if outputformat == 'RGB':
+        RGB = (randint(0,256),randint(0,256),randint(0,256))
+        return RGB
+    else:
+        return choice(['white', 'green', 'blue', 'cyan', 'red', 'yellow', 'orange'])
+
+if __name__ == "__main__":
+    print(couleur_aleatoire())
+    RGB = (randint(0,256),randint(0,256),randint(0,256))
+    print(RGB)
