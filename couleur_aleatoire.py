93b3a43d242f3bab4f633add61a112eab533c0b4
Rio <Nesta.loisy@gmail.com>
2021-10-19 11:12:56 +0200

    couleur_aléatoire

--- couleur_aleatoire.py
+++ couleur_aleatoire.py
@@ -1,4 +1,5 @@
 from random import *
+from turtle import Screen
 import turtle
 
 
@@ -7,14 +8,12 @@ def couleur_aleatoire():
     renvoie un triplet de 3 nombres entier compris entre 0 et 255
     Ce triplet correspond à une couleur codée en RVB
     """
-    outputformat = 'test' # RGB or return a string
+    turtle.colormode(255)
+    outputformat = 'RGB' # RGB or return a string
     if outputformat == 'RGB':
-        RGB = (randint(0,256),randint(0,256),randint(0,256))
-        return RGB
+        return randint(0,256),randint(0,256),randint(0,256)
     else:
         return choice(['white', 'green', 'blue', 'cyan', 'red', 'yellow', 'orange'])
 
 if __name__ == "__main__":
     print(couleur_aleatoire())
-    RGB = (randint(0,256),randint(0,256),randint(0,256))
-    print(RGB)
